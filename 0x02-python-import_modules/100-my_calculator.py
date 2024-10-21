576bc3525d4e8c1cf1e409919aba800d1e390e2b
joel-kudamo <kudamojoel@gmail.com>
2024-10-21 15:42:39 -0700

    import all functions from a file and handle all basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-from sys import argv, exit
+from sys import argv
 if __name__ == "__main__":
-    if len(argv) != 4:
+    num_of_args = len(argv)
+    if num_of_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(argv[1])
