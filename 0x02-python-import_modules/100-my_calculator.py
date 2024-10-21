f07cadac44e9c7d754e0eb20ccc854ae8b52b7f5
joel-kudamo <kudamojoel@gmail.com>
2024-10-21 16:00:33 -0700

    import all the functions from a file and handle basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
     num_of_args = len(argv)
     if num_of_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     operator = argv[2]
     b = int(argv[3])
     if operator == "+":
-        print("{} {} {}= {}".format(a, operator, b, add(a, b)))
+        print(f"{a} {operator} {b}= {add(a, b)}")
     elif operator == "-":
-        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+        print(f"{a} {operator} {b} = {sub(a, b)}")
     elif operator == "*":
-        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+        print(f"{a} {operator} {b} = {mul(a, b)}")
     elif operator == "/":
-        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+        print(f"{a} {operator} {b} = {div(a, b)}")
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
