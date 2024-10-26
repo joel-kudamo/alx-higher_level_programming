06dcb4160914288086424ae3e75626610bfa2a88
joel-kudamo <kudamojoel@gmail.com>
2024-10-25 17:12:37 -0700

    update class Square by adding public method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,3 +34,20 @@ class Square(Rectangle):
                                                          self.x,
                                                          self.y,
                                                          self.width)
+    def update(self, *args, **kwargs):
+        """update square props
+        """
+        if len(args):
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, value)
