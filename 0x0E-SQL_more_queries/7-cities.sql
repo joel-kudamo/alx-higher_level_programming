c72bcc9d10df52cf35f3b4029442d8725b6173fa
joel-kudamo <kudamojoel@gmail.com>
2024-11-21 15:50:59 -0800

    create a database hbtn_0d_usa and table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- creates database hbtn_0d_usa
 -- creates table cities in the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
