c467179d62e911e7d1c58a891ac03f324a4aded6
joel-kudamo <kudamojoel@gmail.com>
2024-11-21 01:24:53 -0800

    List all records with a score >= 10 in the table second_table

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- List all records with a score >= 10 in the table second_table
+-- Results should display both the score and the name in that order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
