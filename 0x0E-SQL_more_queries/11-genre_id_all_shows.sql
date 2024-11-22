b328dd655a62d68a1411ecf97b0b75d9e0af9599
joel-kudamo <kudamojoel@gmail.com>
2024-11-21 16:41:17 -0800

    list all the shows contained in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- lists all shows contained in the database hbtn_0d_tvshows
+-- results sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT s.title, g.genre_id FROM tv_shows AS s LEFT JOIN tv_show_genres AS g ON s.id = g.show_id ORDER BY s.title, g.genre_id;
