184392fd00fcb7b48ddb68ac8fce2a9276251026
joel-kudamo <kudamojoel@gmail.com>
2024-11-21 16:15:19 -0800

    list all cities contained in the database

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists alll cities contained in the database
-SELECT c.id, c.name, s.name FROM cities AS c INNER JOIN states AS s ON c.stat_id = s.id ORDER BY c.id;
+SELECT c.id, c.name, s.name FROM cities AS c INNER JOIN states AS s ON c.state_id = s.id ORDER BY c.id;
