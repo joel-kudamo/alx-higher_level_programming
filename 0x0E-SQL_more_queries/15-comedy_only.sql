29fefff31e5c2c2f153ff539b6442e037fe43024
joel-kudamo <kudamojoel@gmail.com>
2024-11-21 17:29:38 -0800

    list all Comedy shows in the database

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,3 @@
+-- lists all the Comedy shows in the database hbtn_0d_tvshows
+-- results sorted in ascending order by the show title
+SELECT t.title FROM tv_shows AS t INNER JOIN tv_show_genres AS s ON t.id = s.show_id INNER JOIN tv_genres AS g ON g.id = s.genre_id WHERE g.name = "Comedy" ORDER BY t.title;
